636e79f17cb6e74488d05441c75b3b8bffcc415f
Mário Antunes <mariolpantubes@gmail.com>
2020-01-03 01:37:49 +0000

    Added code for predict webpage

--- arduino/ESP8266_MAX7219_CLOCK/ESP8266_MAX7219_CLOCK.ino
+++ arduino/ESP8266_MAX7219_CLOCK/ESP8266_MAX7219_CLOCK.ino
@@ -1,11 +1,18 @@
 #include <SPI.h>
-#include <NTPClient.h>
+#include <Wire.h>
+#include <BH1750.h>
 #include <ESP8266WiFi.h>
+#include <TimeLib.h>
 #include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <Timezone.h>
 #include <Adafruit_GFX.h>
 #include <Max72xxPanel.h>
-#include <TimeLib.h>
-#include <Timezone.h>
+
+#define AMBIENT_LOW 20
+#define AMBIENT_HIGH 3000
+#define INTENSITY_LOW 0
+#define INTENSITY_HIGH 15
 
 int pinCS = D8;
 int numberOfHorizontalDisplays = 4;
@@ -17,8 +24,13 @@ Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVe
 unsigned int wait = 70;
 unsigned int spacer = 1;
 unsigned int width  = 5 + spacer;
-const char* ssid = "Sunset Hackathon";
-const char* password = "sunset2019";
+const char* ssid = "IoT-privacy";
+const char* password = "deadpool2";
+
+float m = float(INTENSITY_LOW - INTENSITY_HIGH) / float(AMBIENT_LOW - AMBIENT_HIGH),
+b = float(INTENSITY_LOW - m) * AMBIENT_LOW;
+
+BH1750 lightMeter;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pt.pool.ntp.org");
@@ -30,11 +42,18 @@ Timezone tz(dstRule, stdRule);
 void setup() {
   Serial.begin(115200);
   delay(100);
+
+  // BH1750
+  Wire.begin();
+  lightMeter.begin();
+  Serial.println("BH1750 Test");
+  Serial.println();
+  delay(100);
   
   WiFi.begin(ssid,password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(100);
     Serial.print(".");
   }
   Serial.println("");
@@ -46,39 +65,60 @@ void setup() {
 
   timeClient.begin();
   
-  matrix.setIntensity(15);      // Use a value between 0 and 15 for brightness
+  matrix.setIntensity(6);      // Use a value between 0 and 15 for brightness
   matrix.setRotation(0, 1);    // The first display is position upside down
   matrix.setRotation(1, 1);    // The first display is position upside down
   matrix.setRotation(2, 1);    // The first display is position upside down
   matrix.setRotation(3, 1);    // The first display is position upside down
 }
 
-void loop() {
-  matrix.fillScreen(LOW);
+unsigned int lux_2_intensity(float lux) {
+  unsigned int rv = 0;
+  
+  if(lux < AMBIENT_LOW) {
+    rv = 0;
+  } else if (lux > AMBIENT_HIGH) {
+    rv = 15;
+  } else {
+    float res = lux*m+b;
+    rv = (unsigned int) round(res);
+  }
 
-  // Update NTP client
+  return rv;
+}
+
+void loop() {
+  unsigned long StartTime = millis();
   timeClient.update();
   unsigned long ts = tz.toLocal(timeClient.getEpochTime());
   char buff[16];
   sprintf(buff, "%.2d:%.2d:%.2d", hour(ts), minute(ts), second(ts));
   String time = String(buff);
   time.trim();
+  time.toCharArray(time_value, 10);
+  Serial.print("Time = ");
   Serial.println(time);
-  time.toCharArray(time_value, 10); 
+  matrix.setIntensity(lux_2_intensity(lightMeter.readLightLevel()));
+  matrix.fillScreen(LOW);
   matrix.drawChar(2,1, time_value[0], HIGH,LOW,1); // H
   matrix.drawChar(8,1, time_value[1], HIGH,LOW,1); // HH
   matrix.drawChar(14,1,time_value[2], HIGH,LOW,1); // HH:
   matrix.drawChar(20,1,time_value[3], HIGH,LOW,1); // HH:M
   matrix.drawChar(26,1,time_value[4], HIGH,LOW,1); // HH:MM
-  matrix.write(); // Send bitmap to display
-  delay(500);
+  matrix.write();
+  unsigned long CurrentTime = millis();
+  unsigned long ElapsedTime = CurrentTime - StartTime;
+  delay(500-ElapsedTime);
 
+  StartTime = millis();
   matrix.fillScreen(LOW);
   matrix.drawChar(2,1, time_value[0], HIGH,LOW,1); // H
   matrix.drawChar(8,1, time_value[1], HIGH,LOW,1); // HH
-  matrix.drawChar(14,1,' ', HIGH,LOW,1);                   // HH:
-  matrix.drawChar(20,1,time_value[3], HIGH,LOW,1); // HH:M
-  matrix.drawChar(26,1,time_value[4], HIGH,LOW,1); // HH:MM
+  matrix.drawChar(14,1,' ',           HIGH,LOW,1); // HH
+  matrix.drawChar(20,1,time_value[3], HIGH,LOW,1); // HH M
+  matrix.drawChar(26,1,time_value[4], HIGH,LOW,1); // HH MM
   matrix.write();
-  delay(500);
+  CurrentTime = millis();
+  ElapsedTime = CurrentTime - StartTime;
+  delay(500-ElapsedTime);
 }
--- webpages/js/predict.js
+++ webpages/js/predict.js
@@ -1,16 +1,38 @@
+function indexOfMax(arr) {
+  if (arr.length === 0) {
+      return -1;
+  }
+
+  var max = arr[0];
+  var maxIndex = 0;
+
+  for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+          maxIndex = i;
+          max = arr[i];
+      }
+  }
+
+  return maxIndex;
+}
+
 class TSM {
-  constructor(w, h, min, max, p) {
+  constructor(w, h, min, max) {
     this.w = w;
     this.h = h;
     this.min = min;
     this.max = max;
-    this.p = p;
-    this.ts0 = 0;
-    this.matrix = [];
-    for(var i=0; i<w; i++) {
-      this.matrix[i] = new Array(h);
-      for(var j=0; j<h; j++) {
-        this.matrix[i][j] = new Array(h);
+    this.ts = 0;
+    this.probs = [];
+    this.previous_value = null;
+    this.total = 0;
+    for (let i = 0; i < w; i++) {
+      this.probs[i] = [];
+      for (let j = 0; j < h; j++) {
+        this.probs[i][j] = []
+        for (let k = 0; k < h; k++) {
+          this.probs[i][j][k] = 0;
+        }
       }
     }
   }
@@ -23,39 +45,226 @@ class TSM {
     return this.h;
   }
 
+  value_bin(value) {
+    let range = this.max - this.min;
+    let inc = range / this.h;
+    let idx = 0;
 
-  computeR(value) {
-    var dp = value > this.max ? this.max : value;
-    var dp = dp < this.min ? this.min : dp;
-    return Math.floor(dp/((this.max-this.min)/this.h))-1;
+    for (let i = this.min; (i + inc) < value; i += inc) {
+      ++idx;
+    }
+
+    return idx;
   }
 
-  computeC(ts) {
-    var dt = ts-Math.floor(ts/this.p)*this.p;
-    return Math.floor(dt/this.w)-1;
+  average_value(k) {
+    let range = this.max - this.min;
+    let inc = range / this.h;
+    let rv = this.min;
+
+    for (let i = 0; i < k; i += 1) {
+      rv += inc;
+    }
+
+    return rv + (inc / 2.0);
   }
 
-  // does not use ts1 right now
-  // it may overflow
-  addPoint(ts0, v0, ts1, v1) {
-    var r = computeR(v0);
-    var c = computeC(ts0);
-    var w = computeR(v1);
-    this.matrix[r][c][w] = this.matrix[r][c][w]+1.0;
+  learn(value) {
+    // Update the max and min variables
+    if (value > this.max) {
+      this.max = value;
+    } else if (value < this.min) {
+      this.min = value;
+    }
+
+    // compute right bin
+    let idx = this.value_bin(value);
+    if (this.previous_value != null) {
+      this.probs[this.ts][this.previous_value][idx]++;
+      //console.log('learn v2 = '+this.probs[this.ts][this.previous_value][idx]);
+      this.ts = (this.ts + 1) % this.w;
+      this.total++;
+    }
+
+    // update time for next point
+    this.previous_value = idx;
   }
-  
-  
+
+  predict() {
+    let i = this.ts;
+    let j = this.previous_value;
+    let k = indexOfMax(this.probs[i][j]); //.reduce((iMax, x, i, this.probs[i][j]) => x > this.probs[i][j][iMax] ? i : iMax, 0);
+    return this.average_value(k);
+  }
 
   toString() {
-    var line=new Array(this.h);
-    for(var i=0; i<this.h; i++) {
-      var pr=this.matrix[this.w-1][i];
-      for(var j=0; j<this.h; j++) {
-        
+    let str = '';
+    // Header
+    str += 'W = ' + this.w + ' H = ' + this.h + ' Min = ' + this.min + ' Max = ' + this.max + '\n';
+    // Probs
+    for (let i = 0; i < this.w; i++) {
+      for (let j = 0; j < this.h; j++) {
+        for (let k = 0; k < this.h; k++) {
+          let v = 0;
+          if (this.total > 0) {
+            v = this.probs[i][j][k] / this.total;
+          }
+          str += 'Probs[' + i + '][' + j + '][' + k + '] = ' + v.toFixed(2) + '\n';
+        }
       }
     }
 
-    for()
-    return "Test toString...";
+    return str;
   }
+}
+
+function setupHighcharts(w, tsm) {
+  Highcharts.setOptions({ global: { useUTC: false } });
+  let today = new Date();
+  let chart = Highcharts.chart('container', {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: 'Humidity'
+    },
+    xAxis: {
+      type: 'datetime',
+      tickPixelInterval: 150
+    },
+    yAxis: {
+        min: 0,
+        max: 100,
+        labels: {
+          format: '{value}%',
+          style: {
+            color: '#7cb5ec'
+          }
+        },
+        title: {
+          text: 'Humidity',
+          style: {
+            color: '#7cb5ec'
+          }
+        }
+      },
+    legend: {
+      enabled: false
+    },
+    exporting: {
+      enabled: false
+    },
+    series: [
+      {
+        name: 'Humidity',
+        type: 'spline',
+        yAxis: 0,
+        color: '#7cb5ec',
+        data: (function () {
+          let data = [], time = today.getTime(), i;
+          for (i = 0; i < w; i += 1) {
+            data.push({ x: (time - ((w - i) * 1000)), y: 50 });
+          }
+          return data;
+        }())
+      }]
+  });
+
+  let predicted = Highcharts.chart('container_predicted', {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: 'Predicted Humidity'
+    },
+    xAxis: {
+      type: 'datetime',
+      tickPixelInterval: 150
+    },
+    yAxis: {
+        min: 0,
+        max: 100,
+        labels: {
+          format: '{value}%',
+          style: {
+            color: '#7cb5ec'
+          }
+        },
+        title: {
+          text: 'Humidity',
+          style: {
+            color: '#7cb5ec'
+          }
+        }
+      },
+    legend: {
+      enabled: false
+    },
+    exporting: {
+      enabled: false
+    },
+    series: [
+      {
+        name: 'Humidity',
+        type: 'spline',
+        yAxis: 0,
+        color: '#7cb5ec',
+        data: (function () {
+          
+          let data = [], time = today.getTime(), i;
+          for (i = 0; i < w; i += 1) {
+            data.push({ x: (time - ((w - i) * 1000)), y: 50 });
+          }
+          return data;
+        }())
+      }]
+  });
+
+  for (let i = 0; i < w; i += 1) {
+    tsm.learn(50);
+  }
+
+  predicted.series[0].addPoint([today.getTime(), tsm.predict()], true, true)
+
+  return [chart, predicted];
+}
+
+function setupWS(charts, ws, tsm) {
+  var ws = new WebSocket(ws);
+
+  ws.onopen = function () {
+    console.log("Subscribe topics: temperature, humidity and pressure.");
+    ws.send("{\"type\":\"sub\",\"topic\":\"humidity\"}");
+  };
+
+  ws.onmessage = function (evt) {
+    var received_msg = evt.data;
+    console.log(received_msg);
+    var json = JSON.parse(received_msg);
+    var ts = json.ts * 1000;
+    switch (json.topic) {
+      case "humidity":
+        charts[0].series[0].addPoint([ts, json.value], true, true);
+        tsm.learn(json.value);
+        charts[1].series[0].addPoint([ts, tsm.predict()], true, true);
+        break;
+    }
+  };
+
+  ws.onclose = function () {
+    console.log("Connection is closed...");
+  };
+}
+
+function checkHC(ws, tsm) {
+  if (typeof Highcharts == 'undefined') {
+    setTimeout(function () { checkHC(ws) }, 1000);
+  } else {
+    setupWS(setupHighcharts(10, tsm), ws, tsm);
+  }
+}
+
+function setup(ws) {
+  let tsm = new TSM(5, 10, 0, 100);
+  checkHC(ws, tsm);
 }
